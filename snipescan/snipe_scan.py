dc6690457a1c9a2351e18c41d6d82a3fd77f2360
James McNutt <azmcnutt@gmail.com>
2024-01-22 06:35:47 -0700

    Sync

---
+++ snipescan/snipe_scan.py
@@ -0,0 +1,12 @@
+"""A simple program to import assets into SnipeIT"""
+
+
+def main():
+    """Main function to load the program and get things started."""
+
+    #let's setup logging
+
+
+
+if __name__ == '__main__':
+    main()
